cf30a4c3709bdde32f826f86238b398c2daa3979
kounosu <kounosu1771036@gmail.com>
2024-04-22 12:24:33 +0900

    ハンバーガーメニューの追加ができればHeaderは完成なんですが、少し詰まっているので先にFooter部分を実装します

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
-import Header from "@/components/atoms/Header/Header";
+import Header from "@/components/atoms/layout/Header";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div className="bg-white">
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0"
+        />
+      </Head>
       <Header />
+      <main>
+        <div>
+          <section></section>
+          <section></section>
+          <section></section>
+          <section></section>
+          <section></section>
+          <section></section>
+        </div>
+      </main>
     </div>
   );
 }
---
+++ src/components/atoms/layout/Header.tsx
@@ -0,0 +1,66 @@
+//0422 ハンバーガーメニューだけ
+
+
+import Link from "next/link";
+
+type MenuItem = {
+  href: string;
+  name: string;
+};
+
+const Header = () => {
+  const menuItems: MenuItem[] = [
+    { name: "Top", href: "/" },
+    { name: "NCAジュニアストリートクラブとは", href: "/" },
+    { name: "コンディショニングとは", href: "/" },
+    { name: "開発者・有吉与志恵について", href: "/" },
+    { name: "実践しているアスリートたち", href: "/" },
+    { name: "サービス内容", href: "/" },
+    { name: "料金", href: "/" },
+    { name: "利用者の声", href: "/" },
+    { name: "オフィシャルサポーター", href: "/" },
+    { name: "NEWS", href: "/" },
+    { name: "よくある質問・お問い合わせ", href: "/" },
+  ];
+
+  return (
+    // <header className="w-[350px] h-screen h-full py-6 px-5 bg-white border-b-8 border-red-500 border-primary flex flex-col justify-start items-center shadow-right">
+    <header className="w-full md:h-full md:w-[350px] z-2 min-w-[350px] fixed z-[99] w-[350px] h-screen py-6 px-5 bg-white border-b-8 border-red-500 flex flex-col justify-start items-center shadow-right">
+      <div className="flex flex-col items-center w-full mb-10  inline-flex">
+        <div className="w-[150px] h-27 justify-center items-center flex">
+          <Link href="/">
+            <img src="/images/logo.jpg" alt="logo" />
+          </Link>
+        </div>
+        <div className="my-5"></div>
+        {menuItems.map((item, index) => (
+          <div
+            key={index}
+            className={`text-[15px] margin-div ${
+              index === 0 ? "border-t" : ""
+            } border-b border-red-500 border-primary self-stretch text-center`}
+          >
+            <Link className="text-zinc-950 grow" href={item.href}>
+              {item.name}
+            </Link>
+          </div>
+        ))}
+        <div className="my-5"></div>
+        <div className=" text-sm border-t border-b self-stretch text-center bg-gradient-to-r from-red-500 via-red-500/50 to-red-500/10 py-2.5">
+          <span className="material-symbols-outlined">edit_note</span>{" "}
+          <Link className="text-white text-[20px]" href="/">
+            新規会員登録
+          </Link>
+        </div>
+        <div className="my-1"></div>
+        <div className="text-sm border-t border-b self-stretch text-center bg-gradient-to-r from-red-500 via-red-500/50 to-red-500/10 py-2.5">
+          <Link className="text-white text-[20px]" href="/">
+            ログイン
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
